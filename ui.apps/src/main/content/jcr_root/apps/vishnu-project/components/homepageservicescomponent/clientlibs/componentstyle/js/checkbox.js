e71268edbc1f41b673d15d17ea3d10601413d8e8
vishnu prasad <vishnu@gmail.com>
2023-06-13 20:19:17 +0530

    enhancement/TRAINING/HomePageServicesComponent/dialog-checkbox-showhide-field

---
+++ ui.apps/src/main/content/jcr_root/apps/vishnu-project/components/homepageservicescomponent/clientlibs/componentstyle/js/checkbox.js
@@ -0,0 +1,67 @@
+/**
+ * How to use:
+ *
+ * 1. Add class to checkbox
+ *
+ *		Example: granite:class="cq-dialog-checkbox-showhide"
+ *
+ * 2. Add cq-dialog-checkbox-showhide-target data-attribute to checkbox with the value being the selector to target for toggleing
+ *
+ *		Example: cq-dialog-checkbox-showhide-target=".togglefield"
+ *
+ * 3. Add target class to toggleable fields or components
+ *
+ *	    Example: granite:class="togglefield"
+ */
+(function (document, $) {
+    "use strict";
+
+    // when dialog gets injected
+    $(document).on("foundation-contentloaded", function (e) {
+        // if there is already an inital value make sure the according target element becomes visible
+            checkboxShowHideHandler($(".cq-dialog-checkbox-showhide", e.target));
+    });
+
+    $(document).on("change", ".cq-dialog-checkbox-showhide", function (e) {
+        checkboxShowHideHandler($(this));
+    });
+
+
+    function checkboxShowHideHandler(el) {
+        el.each(function (i, element) {
+            if($(element).is("coral-checkbox")) {
+                // handle Coral3 base drop-down
+                Coral.commons.ready(element, function (component) {
+                    showHide(component, element);
+                    component.on("change", function () {
+                        showHide(component, element);
+                    });
+                });
+            } else {
+                // handle Coral2 based drop-down
+                var component = $(element).data("checkbox");
+                if (component) {
+                    showHide(component, element);
+                }
+            }
+        })
+    }
+
+    function showHide(component, element) {
+        console.log('showing');
+        console.log("elements : "+$(component).closest(element).data("cqDialogCheckboxShowhideTarget"))
+        // get the selector to find the target elements. its stored as data-.. attribute
+        var target = $(element).closest(component).data("cqDialogCheckboxShowhideTarget");
+        console.info("el :"+element);
+        console.info("el :"+target);
+        //var $target = $(target);
+        var $target = $(element).closest("coral-multifield-item").find(target);
+        if (target) {
+            $target.addClass("hide");
+            if (component.checked) {
+                $target.removeClass("hide");
+            }
+        }
+
+    }
+})(document, Granite.$);
