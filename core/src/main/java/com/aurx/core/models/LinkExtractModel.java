5097d69880503660078b6ead4a94e10ccdd512fa
vishnu prasad <vishnu@gmail.com>
2023-06-15 18:07:18 +0530

    REFECTOR/LinkExtractModel

--- core/src/main/java/com/aurx/core/models/LinkExtractModel.java
+++ core/src/main/java/com/aurx/core/models/LinkExtractModel.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * LinkExtractModel is used to extract links from HTML code.
  * This is used to extract the links from the html code
  */
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
